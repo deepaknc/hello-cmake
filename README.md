7444943480bca3887fb147fbcb9cae160a33ec3b
koby becker <beckerkoby@gmail.com>
2015-10-03 18:26:05 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Simple CMake C++ Project
+
+This is a simple c++ template project to use cmake, and tests with boost.
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "sqr.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << sqr(2.0) << std::endl;	
+	return 0;
+}
